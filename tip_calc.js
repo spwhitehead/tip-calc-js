6a6ee5c91fe50cebf6ec50d95ee5f99ee510f5ea
Spencer Whitehead <53270623+spwhitehead@users.noreply.github.com>
2024-05-20 16:54:35 -0600

    Add rounding for calculations

--- tip_calc.js
+++ tip_calc.js
@@ -4,9 +4,9 @@ let tip_rate = prompt("Enter the tip percentage: ");
 let bill_amount_float = parseFloat(bill_amount);
 let tip_rate_float = parseFloat(tip_rate);
 
-let tip = (bill_amount_float * tip_rate_float / 100);
+let tip = Math.round(bill_amount_float * tip_rate_float / 100);
 
-let total = bill_amount_float + tip;
+let total = Math.round(bill_amount_float + tip);
 
 let message = "The bill total was: $" + bill_amount_float.toFixed(2) + "\n" + "The tip amount is: $" + tip.toFixed(2) + "\n" + "The total amount is: $" + total.toFixed(2);
 
